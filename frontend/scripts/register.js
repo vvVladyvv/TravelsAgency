17fad2aa3bb0dd9abf23c6518b2d1e3a1e38dae2
Vladyvv4 <folano0330@gmail.com>
2026-08-31 16:54:29 -0400

    feat(frontend): User register request update, for add image in the body

--- frontend/scripts/register.js
+++ frontend/scripts/register.js
@@ -5,19 +5,26 @@ if (form) {
     //If is true, once the form has been submitted, we extract all value
     form.addEventListener("submit", async (e) => {
         e.preventDefault();
+
+        const user_data = new FormData()
+
         const username = document.getElementById("username").value;
         const age = Number(document.getElementById("age").value);
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
+        const image = document.getElementById("image").files[0]
+
+        user_data.append("username", username)
+        user_data.append("age", age)
+        user_data.append("email", email)
+        user_data.append("password", password)
+        user_data.append("image", image)
 
             //Post request to the register endpoint, with all data
         try {
             const response = await fetch("/user/register", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ username, age, email, password })
+                body: user_data
             });
             //We store data and then convert it to JSON
             const data = await response.json();
