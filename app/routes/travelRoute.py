e207219eed660146df763c9bd342c8c9124e4697
Vladyvv4 <folano0330@gmail.com>
2026-09-01 11:34:44 -0400

    add(comments): Write code documentation in users route  and travel route

--- app/routes/travelRoute.py
+++ app/routes/travelRoute.py
@@ -1,33 +1,52 @@
+#Fastapi modules
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, Form, File
-#learn this-----------------
+#Librarys for interact with filesystem
 from pathlib import Path
 import shutil
+#Library for generate UID
 import uuid
-#-----------------
+#SQLALCHEMY And Models
 from app.models.db_travels import Travels
-from app.schemas.travelSchemas import CreateTravel, TravelEdit, Reserve
 from sqlalchemy.orm import Session
+#Dependency injection
 from app.database import get_db
-from app.security.authorization import get_current_user, get_admin
+#Schemas
+from app.schemas.travelSchemas import CreateTravel, TravelEdit, Reserve
+#Services and repository
 from app.services.travelServices import create_travel, travel_edit
-from app.services.booking_services import reserve_travel 
-from app.exceptions.userExceptions import AdminRequired
 from app.repositories.Travels_repositories import TravelRepository
+from app.services.booking_services import reserve_travel 
+
+#Auth flow and exceptions handle
+from app.security.authorization import get_current_user, get_admin
+from app.exceptions.userExceptions import AdminRequired
 
 
 
-travel_repository = TravelRepository()
+
+
+
+#Api config for travels 
+
+#Travels route for public services
 travels = APIRouter(prefix="/travel", tags=["Travels Service"])
+#Travels route for travel maintain services(only admin role)
 travels_maintain = APIRouter(prefix="/travel_maintain", tags=["Travels Maintain"])
 
+
+#travel repository for execute querys in the db
+travel_repository = TravelRepository()
 
 #Create a path object and if not exist create uploads folder
 upload_dir = Path("uploads")
 upload_dir.mkdir(exist_ok=True)
 
-#Learn this-------------------------------
+#------------- Travels endpoints (public services) ---------------------
+
+#Endpoint for create a new travel in db
 @travels.post("/create_travel")
 async def new_travel(destination: str = Form(...), activity: str = Form(...) , price: int = Form(...), available_seats: int = Form(...), duration: int = Form(...), image: UploadFile = File(...), admin = Depends(get_admin) , db: Session = Depends(get_db)):
+    #Verify size of the file
     if admin:
         max_size = 5 + (1024 * 1024)
         size = await image.read()
@@ -36,23 +55,20 @@ async def new_travel(destination: str = Form(...), activity: str = Form(...) , p
                 status_code=413,
                 detail="File too large"
             )
-        #FIrst extract extension from the file and then generate and uuid and mix it with our extension
+        #Extract suffix of file and restrict 3 types of suffix
         extension = Path(image.filename).suffix
         if extension not in [".jpg", ".png", ".jpeg"]:
             raise HTTPException(
                 status_code=400,
                 detail="Format supported is (.jpg, .jpeg, .png)"
             )
+        #Generate file name
         unique_name = f"{uuid.uuid4()}{extension}"
-
-        #Create a file path for can access
+        #Generate path of the file
         file_path = upload_dir / unique_name
 
-
-
-        #then we open the content with "wb" paremeters for can write in binary mode
+        #Open file as buffer with "write" and "bytes" permissions, for modify it, then copy content in image inside our buffer object
         with file_path.open("wb") as buffer:
-            #Extract the binary code inside our upload image, and paste it inside our new image
             shutil.copyfileobj(image.file, buffer)
 
        
@@ -67,20 +83,22 @@ async def new_travel(destination: str = Form(...), activity: str = Form(...) , p
 
         return create_travel(travel_data, image.filename, db)
     raise AdminRequired()
-    
-#-----------------------------------------------
 
+#Endpoint for reserve a travel
 @travels.post("/reserve_travel")
 def Reserve_Travel(data:Reserve, user_id = Depends(get_current_user), db: Session = Depends(get_db)):
         booking = reserve_travel(data, db, user_id["user_id"])
         return booking
 
-
-@travels_maintain.get("/get_travels")
+#Endpoint for get all travels
+@travels.get("/get_travels")
 def get_travels(db: Session = Depends(get_db)):
     travels = db.query(Travels).all()
     return travels
 
+#--------------Travels maintain routes (Admin only) ----------------
+
+#Get a specific travel in db
 @travels_maintain.post("/get_travel")
 def get_travel(travel_id: int, admin = Depends(AdminRequired), db: Session = Depends(get_db)):
     if admin:
@@ -88,7 +106,7 @@ def get_travel(travel_id: int, admin = Depends(AdminRequired), db: Session = Dep
             
 
 
-
+#Edit a travel
 @travels_maintain.put("/edit_travel")
 def edit_travel(data: TravelEdit, Admin = Depends(get_admin), db: Session = Depends(get_db)):
     if Admin:
@@ -96,6 +114,8 @@ def edit_travel(data: TravelEdit, Admin = Depends(get_admin), db: Session = Depe
         return travel
     raise AdminRequired()
 
+
+#Delete a travel
 @travels_maintain.delete("/delete_travel")
 def delete_travel(id: int, Admin = Depends(get_admin), db: Session = Depends(get_db)):
     if Admin:
--- app/routes/usersRoute.py
+++ app/routes/usersRoute.py
@@ -1,30 +1,57 @@
+#FastAPi modules
 from fastapi import APIRouter, Depends, HTTPException, status, Form, UploadFile, File
-from pathlib import Path
-import uuid
-import shutil
+from fastapi.responses import FileResponse
+#SQL ALCHEMY modules
 from sqlalchemy.orm import Session
+#File System librarys
+from pathlib import Path
+import shutil
+#UID generate (for my image flow)
+import uuid
+#Models
 from app.models.db_user import Users
+#Schemas
 from app.schemas.usersSchemas import UserRegister, UserLogin, UserResponse, LoginResponse, UserEdit
+#Dependency injection 
 from app.database import get_db
+#Services (users, hashing, authorization_flow )
 from app.services.usersServices import add_new_user, verified_user, admin_role
 from app.security.authorization import get_current_user, get_admin
 from app.security.hashing import hashed_password
-from fastapi.responses import FileResponse
+
+#Personality exepctions
 from app.exceptions.userExceptions import AdminRequired, UserNotFound
+#Repository
 from app.repositories.Users_repositories import UserRepository
 
-
+#----------------Api config routes--------------------------
+#Routes for normal users (Endpoints for users flow)
 user = APIRouter(prefix='/user', tags=["User services"])
+#Routes for Admin users (Endpoints for user maintain)
 user_maintain = APIRouter(prefix='/user_maintain', tags=["User Maintain"])
+
+#Object for my repository (Execute query in my db)
 user_tools = UserRepository()
 
+#Path folder for upload image, if not exist it create one
 upload_path = Path("uploads")
 upload_path.mkdir(exist_ok=True)
 
+
+#----------User route endpoints--------------------------
+
+
+#Endpoint for get frontend template in the route "/"
 @user.get("/")
 async def read_index():
     return FileResponse("index.html")
 
+#Endpoint for get current user using jwt auth
+@user.get("/current_user")
+def get_actual_user(user = Depends(get_current_user)):
+    return user
+
+#Endpoint for register new user
 @user.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def register_user(username: str = Form(...), age: int = Form(...), email: str = Form(...), password: str = Form(...), image: UploadFile = File(...), db: Session = Depends(get_db)):
 
@@ -47,11 +74,15 @@ def register_user(username: str = Form(...), age: int = Form(...), email: str = 
     user = add_new_user(new_user, image_uuid, db)
     return user
 
+#Endpoint for authenthicate user
 @user.post("/login", response_model=LoginResponse, status_code=status.HTTP_200_OK)
 def login_user(data: UserLogin, db: Session = Depends(get_db)):
     token = verified_user(data, db)
     return token
 
+#------------ User_maintain routes (only admin role) ---------------------
+
+#Get all users from db
 @user_maintain.get("/get_users", response_model=list[UserResponse])
 def get_users(Admin = Depends(get_admin), db: Session = Depends(get_db)):
     if Admin:
@@ -60,6 +91,7 @@ def get_users(Admin = Depends(get_admin), db: Session = Depends(get_db)):
     raise AdminRequired()
 
 
+#Get a specific user from db
 @user_maintain.post("/get_user", response_model=UserResponse)
 def get_user(user_id: int, admin = Depends(get_admin), db: Session = Depends(get_db)):
     if admin:
@@ -67,6 +99,8 @@ def get_user(user_id: int, admin = Depends(get_admin), db: Session = Depends(get
         return user
     raise AdminRequired()
 
+
+#Edit an exist user 
 @user_maintain.put("/edit_user", response_model=UserResponse)
 def edit_user(userId: int = Form(...), new_username: str = Form(...), new_age: int = Form(...), new_email: str = Form(...), new_password: str = Form(...), new_image: UploadFile = File(...), Admin = Depends(get_admin), db: Session = Depends(get_db)):
 
@@ -105,6 +139,7 @@ def edit_user(userId: int = Form(...), new_username: str = Form(...), new_age: i
     raise AdminRequired()
 
 
+#delete an exist user
 @user_maintain.delete("/delete_user", response_model=UserResponse)
 def delete_user(id: int, Admin = Depends(get_admin), db: Session = Depends(get_db)):
     if Admin:
@@ -117,10 +152,7 @@ def delete_user(id: int, Admin = Depends(get_admin), db: Session = Depends(get_d
         raise HTTPException(status_code=404, detail="User was not found")
     raise AdminRequired()
 
-@user.get("/current_user")
-def get_actual_user(user = Depends(get_current_user)):
-    return user
-
+#User for get admin role
 @user_maintain.post("/get_admin")
 def get_admin_role(id: int, db: Session = Depends(get_db)):
     return admin_role(id, db)
