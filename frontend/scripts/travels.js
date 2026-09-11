abeca15d48b3b384fc98e68ceb80ceab54ff81a7
Vlady <folano0330@gmail.com>
2026-09-11 10:10:23 -0400

    update(frontend): Main page new design

--- frontend/scripts/travels.js
+++ frontend/scripts/travels.js
@@ -1,5 +1,6 @@
 //Get travels container section
 const travel_section = document.getElementById("travels_container")
+const travel_category = document.querySelectorAll(".category_btn")
 
 
 //Get request to obtain travels
@@ -65,3 +66,67 @@ async function get_travels() {
 }
 
 get_travels()
+
+travel_category.forEach(category => {
+    category.addEventListener("click", async () => {
+        console.log("hello word")
+        const response = await fetch(
+            "/travel/get_travels",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+            
+        )
+        const travels = await response.json()
+        travels.forEach(travel => {
+            if (travel.activity.toLowerCase() == category.innerText.toLowerCase()) {
+                //Create travel_Card container with class "travel_card"
+                const card = document.createElement("div")
+                card.className = "travel_card"
+
+                //little container for info into the card
+                const card_information = document.createElement("div")
+                card_information.className = "card_information"
+
+                //Inside the info container we create tittle and button
+                const travel_destination = document.createElement("h2")
+                travel_destination.innerText = travel.destination
+
+                const travel_button = document.createElement("button")
+                travel_button.innerText = "See more"
+                travel_button.className = "card_button"
+
+                //Then create a new container inside the card for image
+                const image_container = document.createElement("div")
+                image_container.className = "card_image"
+
+                const image = document.createElement("img")
+                image.src = `uploads/${travel.image}`
+                image_container.append(image)
+
+
+                card_information.append(travel_destination)
+                card_information.append(travel_button)
+
+                card.append(image_container)
+                card.append(card_information)
+
+                //Putting all containers inside the travel section (Main container)
+                travel_section.append(card)
+
+                //When mouse are hover the card(image) this show hidde info
+                image_container.addEventListener("mouseenter", () => {
+                    card_information.classList.add("show")
+                })
+                //But when mouse leave the card(image) this info is hidden again
+                image_container.addEventListener("mouseleave", () => {
+                    card_information.classList.remove("show")
+                })
+                }
+            
+        });
+
+})})
